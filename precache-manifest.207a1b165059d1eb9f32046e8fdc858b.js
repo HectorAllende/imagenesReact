55a737081a11f004614623a2b495baed49fbc3ee
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-10-13 21:14:51 -0300

    reactimagenes

---
+++ precache-manifest.207a1b165059d1eb9f32046e8fdc858b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e0f3c7a6234a3e69214a9bb31ac406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eacde6c20ff22a89141b",
+    "url": "/static/js/2.ab265026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab265026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc93d2246640cbf46bd1",
+    "url": "/static/js/main.91fb931f.chunk.js"
+  },
+  {
+    "revision": "a176d22fca1925425e16",
+    "url": "/static/js/runtime-main.e93158d6.js"
+  }
+]);
